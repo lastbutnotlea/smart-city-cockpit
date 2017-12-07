fffdb5ce2d0e36ae89811629dab25e6513cb331e
Lea R <n/a>
2017-12-07 14:43:42 +0100

    setup gradle keine ahnung

--- crocodile/src/main/java/de/team5/super_cute/crocodile/crocodile/CrocodileApplication.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/crocodile/CrocodileApplication.java
@@ -1,4 +1,4 @@
-package de.team5.super.cute.crocodile.crocodile;
+package de.team5.super_cute.crocodile.crocodile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
