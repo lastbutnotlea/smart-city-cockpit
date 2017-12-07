2f57841fcac03cf595caeb6e8d5a7cda12a824f9
Lea R <n/a>
2017-12-07 13:09:21 +0100

    initial project setup

---
+++ crocodile/src/main/java/de/team5/super/cute/crocodile/crocodile/CrocodileApplication.java
@@ -0,0 +1,12 @@
+package de.team5.super.cute.crocodile.crocodile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrocodileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrocodileApplication.class, args);
+	}
+}
