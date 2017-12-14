aeb5fedf50b6de3888689aa6d67eb74e80fcd346
NameThatDoesNotExistAlready <monika.pichlmair@student.uni-augsburg.de>
2017-12-14 23:50:16 +0100

    first draft drawing lines

--- charlie/src/app/view/network/network.component.ts
+++ charlie/src/app/view/network/network.component.ts
@@ -2,6 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {HttpRoutingService} from '../../services/http-routing.service';
 import { LineData } from '../../shared/line-data';
 
+import * as d3 from 'd3-selection';
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
+
+import { Station, Stations } from './stations';
+import { Line, Lines } from './lines';
+import { Connection, Connections } from './connections';
+
 @Component({
   selector: 'app-network-view',
   templateUrl: './network.component.html',
@@ -12,19 +22,40 @@ export class NetworkComponent implements OnInit {
   title: String;
   lines: LineData[] = [];
 
+  private margin = {top: 20, right: 20, bottom: 30, left: 50};
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+
+  private stations: Station[] = [];
+  private tubes: Line[] = [];
+  private connections: Connection[] = [];
+
   constructor(private http: HttpRoutingService) { }
 
   ngOnInit(): void {
     this.title = 'network view';
     // get line data
-    this.http.getLines().subscribe( data => {
+    /*this.http.getLines().subscribe( data => {
         this.lines = data;
       },
       err => {
         console.log('Could not fetch lines.');
       }
-    );
+    );*/
+
+    this.stations = Stations;
+    this.connections = Connections;
+    this.tubes = Lines;
+
+    this.initSvg();
+    this.initAxis();
+    this.drawAxis();
+    this.drawLine();
   }
+
 
   /**
    * Returns true if lines contains at least one line, false otherwise
@@ -37,4 +68,74 @@ export class NetworkComponent implements OnInit {
     return false;
   }
 
+  private initSvg() {
+    this.width = 900 - this.margin.left - this.margin.right;
+    this.height = 500 - this.margin.top - this.margin.bottom;
+    this.svg = d3.select('svg')
+      .append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  }
+
+  private initAxis() {
+    const minLat = d3Array.min(this.stations, function(d) {return d.latitude; });
+    const minLon = d3Array.min(this.stations, function(d) {return d.longitude; });
+    const maxLat = d3Array.max(this.stations, function(d) {return d.latitude; });
+    const maxLon = d3Array.max(this.stations, function(d) {return d.longitude; });
+
+    this.x = d3Scale.scaleLinear().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain([minLon, maxLon]);
+    this.y.domain([minLat, maxLat]);
+  }
+
+  private drawAxis() {
+
+    this.svg.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(d3Axis.axisBottom(this.x));
+
+    this.svg.append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3Axis.axisLeft(this.y))
+      .append('text')
+      .attr('class', 'axis-title')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end');
+  }
+
+  private drawLine() {
+
+    this.svg.append('svg:line')
+      .attr('class', 'line')
+      .attr('stroke', '#AE6017')
+      .attr('x1', 1)
+      .attr('y1',  1)
+      .attr('x2', 100)
+      .attr('y2', 100);
+
+    this.connections.forEach(connection => {
+      const s1 = this.getStationById(connection.station1);
+      const s2 = this.getStationById(connection.station2);
+      this.svg.append('svg:line')
+        .attr('class', 'line')
+        .attr('stroke', '#AE6017')
+        .attr('x1', this.x(s1.longitude))
+        .attr('y1', this.y(s1.latitude))
+        .attr('x2', this.x(s2.longitude))
+        .attr('y2', this.y(s2.latitude));
+    });
+  }
+
+  private getStationById(id: number): Station {
+    let result = null;
+    this.stations.forEach(station => {
+      if (station.id === id) {
+        result = station;
+      }
+    })
+    return result;
+  }
 }
