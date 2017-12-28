7d1dd5da051abfd42f751fd0c5e313e3e3dd3d2c
Caro <carolin.brandt@tum.de>
2017-12-28 10:06:44 +0100

    Time set for whole day

--- crocodile/src/main/java/de/team5/super_cute/crocodile/config/InitialSetupConfig.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/config/InitialSetupConfig.java
@@ -7,7 +7,7 @@ public class InitialSetupConfig {
   public static final int fromHour = 0;
   public static final int fromMinute = 0;
   public static final int toHour = 23;
-  public static final int toMinute = 00;
+  public static final int toMinute = 59;
 
   public static final ArrayList<String> lineIds = new ArrayList<String>() {{
     add("10");
