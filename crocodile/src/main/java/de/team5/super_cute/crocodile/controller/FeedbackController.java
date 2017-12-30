75ec6c29ff50d07a68f84043500d9f8812e5a6fa
manu1233 <uu3i6!l2l7Q?>
2017-12-30 09:36:29 +0100

    ...

--- crocodile/src/main/java/de/team5/super_cute/crocodile/controller/FeedbackController.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/controller/FeedbackController.java
@@ -1,11 +1,13 @@
 package de.team5.super_cute.crocodile.controller;
 
 import de.team5.super_cute.crocodile.data.BaseData;
-import de.team5.super_cute.crocodile.model.*;
-
+import de.team5.super_cute.crocodile.model.EFeedbackType;
+import de.team5.super_cute.crocodile.model.Feedback;
+import de.team5.super_cute.crocodile.model.Line;
+import de.team5.super_cute.crocodile.model.Stop;
+import de.team5.super_cute.crocodile.model.Vehicle;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/feedback")
 public class FeedbackController extends BaseController<Feedback> {
 
-    @Autowired
-    public FeedbackController(BaseData<Feedback> feedbackBaseData) {
-        data = feedbackBaseData;
-    }
+  @Autowired
+  public FeedbackController(BaseData<Feedback> feedbackBaseData) {
+    data = feedbackBaseData;
+  }
 
-    @GetMapping
-    public List<Feedback> getAllFeedbacks() {
-        return data.getData();
-    }
+  @GetMapping
+  public List<Feedback> getAllFeedbacks() {
+    return data.getData();
+  }
 
-    @GetMapping("/{vehicleId}")
-    public List<Feedback> getVehicleFeedbacks(@PathVariable String vehicleId) {
-        return data.getData().stream().filter(f -> f.getFeedbackType() == EFeedbackType.VEHICLE_FEEDBACK).filter(f -> ((Vehicle)f.getObjective()).getId() == vehicleId).collect(Collectors.toList());
-    }
+  @GetMapping("/{vehicleId}")
+  public List<Feedback> getVehicleFeedbacks(@PathVariable String vehicleId) {
+    return data.getData().stream()
+        .filter(f -> f.getFeedbackType() == EFeedbackType.VEHICLE_FEEDBACK)
+        .filter(f -> ((Vehicle) f.getObjective()).getId() == vehicleId)
+        .collect(Collectors.toList());
+  }
 
-    @GetMapping("/{stopId}")
-    public List<Feedback> getStopFeedbacks(@PathVariable String stopId) {
-        return data.getData().stream().filter(f -> f.getFeedbackType() == EFeedbackType.STOP_FEEDBACK).filter(f -> ((Stop)f.getObjective()).getId() == stopId).collect(Collectors.toList());
-    }
+  @GetMapping("/{stopId}")
+  public List<Feedback> getStopFeedbacks(@PathVariable String stopId) {
+    return data.getData().stream().filter(f -> f.getFeedbackType() == EFeedbackType.STOP_FEEDBACK)
+        .filter(f -> ((Stop) f.getObjective()).getId() == stopId).collect(Collectors.toList());
+  }
 
-    @GetMapping("/{lineId}")
-    public List<Feedback> getLineFeedbacks(@PathVariable String lineId) {
-        return data.getData().stream().filter(f -> f.getFeedbackType() == EFeedbackType.LINE_FEEDBACK).filter(f -> ((Line)f.getObjective()).getId() == lineId).collect(Collectors.toList());
-    }
+  @GetMapping("/{lineId}")
+  public List<Feedback> getLineFeedbacks(@PathVariable String lineId) {
+    return data.getData().stream().filter(f -> f.getFeedbackType() == EFeedbackType.LINE_FEEDBACK)
+        .filter(f -> ((Line) f.getObjective()).getId() == lineId).collect(Collectors.toList());
+  }
 }
--- crocodile/src/main/java/de/team5/super_cute/crocodile/generator/InitialDataGenerator.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/generator/InitialDataGenerator.java
@@ -179,7 +179,7 @@ public class InitialDataGenerator {
     Vehicle vehicle;
     if (queueFrom.peek() == null || queueFrom.peek().getValue().compareTo(iterator) == 1) {
       //If no (or no available) vehicle exists: create new one
-      vehicle = new Vehicle(100, 50, 28, new HashSet<>(), line.getType());
+      vehicle = new Vehicle(100, 50, 0, 28, new HashSet<>(), line.getType());
       networkDataBuilder.addVehicles(vehicle);
     } else {
       vehicle = queueFrom.poll().getKey();
