d36fea083fd357587721dd718f95346d40b120fd
manu1233 <uu3i6!l2l7Q?>
2018-01-04 14:40:24 +0100

    added LinesForStop()

--- crocodile/src/main/java/de/team5/super_cute/crocodile/controller/StopController.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/controller/StopController.java
@@ -1,9 +1,12 @@
 package de.team5.super_cute.crocodile.controller;
 
 import de.team5.super_cute.crocodile.data.BaseData;
+import de.team5.super_cute.crocodile.data.LineData;
+import de.team5.super_cute.crocodile.model.Line;
 import java.util.List;
 
 import de.team5.super_cute.crocodile.model.Stop;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stops")
 public class StopController extends BaseController<Stop> {
 
+  private LineData lineData;
+
   @Autowired
-  public StopController(BaseData<Stop> stopData) {
+  public StopController(BaseData<Stop> stopData, LineData lineData) {
     data = stopData;
+    this.lineData = lineData;
   }
 
   @GetMapping
@@ -27,5 +33,14 @@ public class StopController extends BaseController<Stop> {
   @GetMapping("/{id}")
   public Stop getStop(@PathVariable String id) {
     return getObjectForId(id);
+  }
+
+  @GetMapping("/{id}/lines")
+  public List<Line> getLinesForStop(@PathVariable String id) {
+    return lineData.getData().stream()
+        .filter(
+            l -> l.getStopsOutbound().stream().anyMatch(s -> s.getId().equals(id)) ||
+                 l.getStopsInbound().stream().anyMatch(s -> s.getId().equals(id)))
+        .collect(Collectors.toList());
   }
 }
