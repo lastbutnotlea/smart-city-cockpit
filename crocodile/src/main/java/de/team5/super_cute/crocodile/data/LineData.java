552c952ca0d9ecf8d4d8bde7a926850d32bf2324
manu1233 <uu3i6!l2l7Q?>
2018-01-04 14:13:40 +0100

    added MapState

--- crocodile/src/main/java/de/team5/super_cute/crocodile/data/LineData.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/data/LineData.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 import de.team5.super_cute.crocodile.model.Stop;
 import de.team5.super_cute.crocodile.model.Trip;
 import de.team5.super_cute.crocodile.util.StateCalculator;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
