72435ed156bf298758fa9f601ea5428f1ba5ce95
manu1233 <uu3i6!l2l7Q?>
2018-01-06 13:33:39 +0100

    added check if vehicle is in use and delete old trips before deleting vehicle

--- crocodile/src/main/java/de/team5/super_cute/crocodile/controller/VehicleController.java
+++ crocodile/src/main/java/de/team5/super_cute/crocodile/controller/VehicleController.java
@@ -1,8 +1,11 @@
 package de.team5.super_cute.crocodile.controller;
 
 import de.team5.super_cute.crocodile.data.BaseData;
+import de.team5.super_cute.crocodile.data.TripData;
+import de.team5.super_cute.crocodile.model.Trip;
 import de.team5.super_cute.crocodile.model.Vehicle;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +24,12 @@ public class VehicleController extends BaseController<Vehicle> {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  private TripData tripData;
+
   @Autowired
-  public VehicleController(BaseData<Vehicle> vehicleData) {
+  public VehicleController(BaseData<Vehicle> vehicleData, TripData tripData) {
     data = vehicleData;
+    this.tripData = tripData;
   }
 
   @GetMapping
@@ -47,6 +53,16 @@ public class VehicleController extends BaseController<Vehicle> {
   @DeleteMapping("/{id}")
   public String deleteVehicle(@PathVariable String id) {
     logger.info("Got Request to delete the vehicle with id " + id);
+    if (tripData.getPresentAndFutureTripsForVehicle(id)) {
+      //there are planned or active trips for that vehicle
+      return "Vehicle is in use!";
+    }
+    //delete past trips for this vehicle
+    List<Trip> trips = tripData.getData().stream().filter(t -> t.getVehicle().getId().equals(id))
+        .collect(Collectors.toList());
+    for (Trip trip:trips) {
+      tripData.deleteObject(trip.getId());
+    }
     return deleteObject(id);
   }
 
