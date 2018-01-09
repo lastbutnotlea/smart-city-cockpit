046142549af9e9826a1cce06328db180b55478cd
Tobias Beeh <tobias.beeh@t-online.de>
2018-01-09 17:59:24 +0100

    added filter

--- charlie/src/app/view/feedback/feedback.component.ts
+++ charlie/src/app/view/feedback/feedback.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpRoutingService} from '../../services/http-routing.service';
 import {FeedbackData} from '../../shared/data/feedback-data';
+import {FilterGroupComponent} from '../../shared/components/filter-group/filter-group.component';
+import {FilterComponent} from '../../shared/components/filter/filter.component';
 
 @Component({
   selector: 'app-feedback',
@@ -11,11 +13,19 @@ export class FeedbackComponent implements OnInit {
   title: string = 'Feedback';
   feedback: FeedbackData[] = null;
 
+  @ViewChild(FilterGroupComponent)
+  filterGroup: FilterGroupComponent;
+
   constructor(private http: HttpRoutingService) {
   }
 
   ngOnInit() {
     this.http.getFeedback().subscribe(data => this.feedback = data);
+    let ratingFilter: FilterComponent = new FilterComponent();
+    ratingFilter.addFilter("Fine", feedback => feedback.rating === "FINE");
+    ratingFilter.addFilter("Problematic", feedback => feedback.rating === "PROBLEMATIC");
+    ratingFilter.addFilter("Critical", feedback => feedback.rating === "CRITICAL");
+    this.filterGroup.addFilterComponent(ratingFilter);
   }
 
 }
