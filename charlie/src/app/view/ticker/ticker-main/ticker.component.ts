30f4ab4669eb53b781632afe800a7d86e7428d24
Tobias Beeh <qwertzuiop2@t-online.de>
2018-01-15 10:35:57 +0100

    removed dead code

--- charlie/src/app/view/ticker/ticker-main/ticker.component.ts
+++ charlie/src/app/view/ticker/ticker-main/ticker.component.ts
@@ -12,13 +12,6 @@ import {LiveDataComponent} from '../../../shared/components/live-data/live-data.
 export class TickerComponent extends LiveDataComponent implements OnInit {
   items: TickerData[] = [];
 
-  public tickerItems: string[] = [
-    'Soccer from 6pm to 7:45pm',
-    'Terrorist attack planned for 8pm',
-    'No power on some stations due to a tsunami',
-    'Smart City Cockpit not operational due to a database issue. Technician is informed.'
-  ];
-
   constructor(private http: HttpRoutingService) {
     super();
   }
