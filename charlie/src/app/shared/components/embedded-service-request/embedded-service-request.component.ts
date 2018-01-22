8ee135345cb4dbea662d1a3e5918b8efeebd188c
NameOfThisUser <monika.pichlmair@student.uni-augsburg.de>
2018-01-22 17:12:01 +0100

    change requests

--- charlie/src/app/shared/components/embedded-service-request/embedded-service-request.component.ts
+++ charlie/src/app/shared/components/embedded-service-request/embedded-service-request.component.ts
@@ -11,4 +11,15 @@ export class EmbeddedServiceRequestComponent{
 
   @Input() serviceRequests: ServiceRequestData[] = [];
 
+  hasOpenRequests(): boolean {
+    return this.serviceRequests.filter(data => this.isOpenRequest(data)).length !== 0;
+  }
+
+  hasClosedRequests(): boolean {
+    return this.serviceRequests.filter(data => !this.isOpenRequest(data)).length !== 0;
+  }
+
+  isOpenRequest(data: ServiceRequestData): boolean {
+    return data.statusCode !== 'COMPLETED' && data.statusCode !== 'CLOSED';
+  }
 }
