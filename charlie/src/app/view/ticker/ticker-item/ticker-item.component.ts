8abe8e939ea7fa68c2badffc96165dab889a555c
Tobias Beeh <qwertzuiop2@t-online.de>
2018-01-25 15:10:01 +0100

    first implementation

--- charlie/src/app/view/ticker/ticker-item/ticker-item.component.ts
+++ charlie/src/app/view/ticker/ticker-item/ticker-item.component.ts
@@ -1,31 +1,55 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {HttpRoutingService} from '../../../services/http-routing.service';
 import {TickerData} from '../../../shared/data/ticker-data';
 import {AppRoutingModule} from '../../../app-routing.module';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-ticker-item',
   templateUrl: './ticker-item.component.html',
-  styleUrls: ['./ticker-item.component.css']
+  styleUrls: ['./ticker-item.component.css'],
 })
-export class TickerItemComponent implements OnInit {
+export class TickerItemComponent {
   @Input()
   data: TickerData;
   @Output()
   onDelete: EventEmitter<TickerData> = new EventEmitter<TickerData>();
 
-  link: string;
+  itemClass: string = 'no-highlight';
+  crossClass: string = 'bg-white';
 
-  constructor(private http: HttpRoutingService) {
+  hoverOnCross: boolean = false;
+
+  constructor(private http: HttpRoutingService, private router: Router) {
   }
 
-  ngOnInit() {
-    this.link = AppRoutingModule.getUrlForId(this.data.item.id);
+  outerHover(flag: boolean): void {
+    if (flag) {
+      this.itemClass = 'highlight';
+    } else {
+      this.itemClass = 'no-highlight';
+    }
+  }
+
+  innerHover(flag: boolean): void {
+    this.hoverOnCross = flag;
+    if (flag) {
+      this.crossClass = '';
+    } else {
+      this.crossClass = 'bg-white';
+    }
   }
 
   deleteItem(): void {
     this.http.deleteTickerItem(this.data).subscribe(
       data => this.onDelete.emit(this.data),
       err => console.log(JSON.stringify(err)));
+  }
+
+  goToLink(): void {
+    if (!this.hoverOnCross) {
+      let link : string = AppRoutingModule.getUrlForId(this.data.item.id);
+      this.router.navigate([link]);
+    }
   }
 }
