51756e9a3831d4b30e24479160e25b8b2981901d
Tobias Beeh <qwertzuiop2@t-online.de>
2018-01-28 14:44:56 +0100

    f*** dropdowns are styled after 2 days of work

--- charlie/src/app/shared/components/dropdown/dropdown.component.ts
+++ charlie/src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,8 +1,13 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, ElementRef, EventEmitter, HostListener, Input, Output,
+  ViewChild
+} from '@angular/core';
+import {DomSanitizer, SafeStyle} from "@angular/platform-browser";
 
 export class DropdownValue {
   readonly value: any;
   readonly label: string;
+
   constructor(value: any, label: string) {
     this.value = value;
     this.label = label;
@@ -21,15 +26,50 @@ export class DropdownComponent {
   @Input()
   selected: DropdownValue;
 
+  @Input()
+  width: number = 200;
+
   @Output()
   selectedChange: EventEmitter<DropdownValue> = new EventEmitter<DropdownValue>();
 
-  constructor() {
-    this.selectedChange.subscribe(value => this.selected = value);
+  @ViewChild('element') component: ElementRef;
+  @ViewChild('dropdownButton') button: ElementRef;
+  isOpen: boolean = false;
+
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  public onClick(targetElement: any) {
+    // close the dropdown if clicked outside
+    const clickedInside = this.component.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.isOpen = false;
+    }
+  }
+
+  getOffsetTop(): string {
+    return this.calculateOffsetTop(this.button.nativeElement) + 'px';
+  }
+
+  calculateOffsetTop(el: any): number {
+    // because of the necessary absolute positioning we need to calculate the position
+    let offset = el.offsetTop;
+    return el.offsetParent ? offset + this.calculateOffsetTop(el.offsetParent) : offset;
+  }
+
+  getPositionStyle(): SafeStyle {
+    return this.domSanitizer.bypassSecurityTrustStyle('top: ' + this.getOffsetTop());
+  }
+
+  getWidthStyle(): SafeStyle {
+    return this.domSanitizer.bypassSecurityTrustStyle('width: ' + this.width + 'px');
   }
 
   select(value: DropdownValue): void {
+    this.selected = value;
     this.selectedChange.emit(value);
+    this.isOpen = false;
   }
 }
 
